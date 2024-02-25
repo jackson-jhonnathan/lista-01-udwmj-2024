3e025517a727f53fd30e10de9fe3239437e7e233
Jackson Jhonnathan <131427349+jackson-jhonnathan@users.noreply.github.com>
2024-02-25 18:29:09 -0300

    aula011.js

---
+++ aula011.js
@@ -0,0 +1,5 @@
+const num1 = 10;
+const num2 = 10;
+const resultado = num1 + num2;
+console.log('A soma dos dois números é', resultado);
+console.log('O resultado da soma divido por dois é', resultado / 2)
